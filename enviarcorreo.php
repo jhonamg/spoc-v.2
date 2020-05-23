e5075c247696b2cf1075970d8ca0da1643e24c42
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-05-23 15:13:48 -0500

    Update enviarcorreo.php

--- enviarcorreo.php
+++ enviarcorreo.php
@@ -170,7 +170,6 @@ for($i = 1; $i <= $totEXH; $i++){
     // echo $query;
     $mysqli->real_query($query9);
     $mysqli->close();
-     $mysqli->close();
     $mysqli = new mysqli("localhost", "root", "", "spoc_bd");
     if ($mysqli->connect_errno) {
       echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
@@ -200,65 +199,85 @@ for($i = 1; $i <= $totEXHComp; $i++){
     $mysqli->close();
 }
 
-echo $mensaje_EDV;
-echo $mensaje_EXH;
-
 
 // $mysqli->close();
 
 
 //-----------------------------------------------------envio de correo---------------------------------//
 
-// $subbject= "ALERTAS - ".$_POST["listatiendas"];
+$mensaje = '';
+if($mensaje_top != ''){
+    $mensaje = "PRECIOS TOP: </br>".$mensaje_top."</br>";
+}
+if($mensaje_EDV != ''){
+    $mensaje = "VISIBILIDAD: </br>".$mensaje_EDV."</br>";
+}
+if($mensaje_EXH != ''){
+    $mensaje = "EXHIBICIONES: </br>".$mensaje_EXH."</br>";
+}
 
-// $body="Nombre: ".$nombre . "</br>Correo: ".$correo . "</br>Telefono: ".$telefono . "</br>Mensaje: ".$mensaje;
+ $mysqli = new mysqli("localhost", "root", "", "spoc_bd");
+    if ($mysqli->connect_errno) {
+      echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }    
+    $query12 = "SELECT tienda.dsc_tienda, tienda.id, ubicacion.dsc_ubicacion FROM tienda INNER JOIN ubicacion ON tienda.id_ubicacion = ubicacion.id WHERE tienda.id = '".$_POST["listatiendas"]."'";
+    // echo $query2;
+    $mysqli->real_query($query12);
+    $resultado = $mysqli->use_result();
+    $fila1 = $resultado->fetch_assoc();
+    $mysqli->close();
+
+$subbject= "ALERTAS - ".$_POST["listatiendas"];
+
+$body="<b>Spoc:</b> ".$id_usuario."</br><b>Tienda:</b> ".$fila1['id']." | ".$fila1['dsc_tienda']." | ".$fila1['dsc_ubicacion']."</br><b>Fecha y hora:</b> ".$fechaActual."</br><b>Mensaje:</b> ".$mensaje;
 
 
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// require 'PHPMailer/Exception.php';
-// require 'PHPMailer/PHPMailer.php';
-// require 'PHPMailer/SMTP.php';
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 
-// // Instantiation and passing `true` enables exceptions
-// $mail = new PHPMailer(true);
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-// try {
-//     //Server settings
-//     $mail->SMTPDebug = 0;                      // Enable verbose debug output
-//     $mail->isSMTP();                                            // Send using SMTP
-//     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-//     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-//     $mail->Username   = 'jhonamg8@gmail.com';                     // SMTP username
-//     $mail->Password   = '01123581321345589';                               // SMTP password
-//     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-//     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'jhonamg8@gmail.com';                     // SMTP username
+    $mail->Password   = '01123581321345589';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-//     //Recipients
-//     $mail->setFrom('jhonamg8@gmail.com', $nombre);
-//     $mail->addAddress('mirellyagv@gmail.com', 'Mirelly');     // Add a recipient
-//     $mail->addAddress('jhonamg8@gmail.com');               // Name is optional
-//     // $mail->addReplyTo('info@example.com', 'Information');
-//     // $mail->addCC($correo);
-//     // $mail->addBCC('bcc@example.com');
+    //Recipients
+    $mail->setFrom('jhonamg8@gmail.com', 'Jhon');
+    $mail->addAddress('mirellyagv@gmail.com', 'Mirelly');     // Add a recipient
+    $mail->addAddress('jhonamg8@gmail.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC($correo);
+    // $mail->addBCC('bcc@example.com');
 
-//     // Attachments
-//     $mail->addAttachment('D:\Xampp\htdocs\spoc-v.2\assets\img\Nueva carpeta\tottus.jpg','imagen de tottus baner');         // Add attachments
-//     // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    // Attachments
+    // $mail->addAttachment('D:\Xampp\htdocs\spoc-v.2\assets\img\Nueva carpeta\tottus.jpg','imagen de tottus baner');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-//     // Content
-//     $mail->isHTML(true);                                  // Set email format to HTML
-//     $mail->Subject = 'administración';
-//     $mail->Body    = $body.'</br>This is the HTML message body <b>in bold!</b>';
-//     $mail->CharSet ='UTF-8';
-//     // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-//     $mail->send();
-//     echo 'Message has been sent';
-//     echo '<script> window.history.go(-1); </script>';
-// } catch (Exception $e) {
-//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-// }
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'ALERTAS - '.$fila1['dsc_tienda'];
+    $mail->Body    = $body.'</br>This is the HTML message body <b>in bold!</b>';
+    $mail->CharSet ='UTF-8';
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    if($mensaje_top != '' || $mensaje_EXH != '' || $mensaje_EDV != ''){
+        $mail->send();
+    }
+    echo 'Message has been sent';
+    echo '<script> window.history.go(-1); </script>';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 
 ?>
